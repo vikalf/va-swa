ff32626a66b18ac18f383b396206d0e99bd6a9dd
Valonso <Valonso@kaplan.edu>
2020-09-13 18:21:55 -0500

    update 1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a test from Azure Static Web Apps
+          This is another test from Azure Static Web Apps ☺
         </p>
         <a
           className="App-link"
